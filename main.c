bf1d75eb11a4e2fbaa0004a90c6685c3f21783c2
thiagolopes <thiagolopes@protonmail.com>
2023-03-21 00:41:43 -0300

    Angle player implementation

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 #include <stdio.h>
+#include <math.h>
 
 #define WIDTH 1024
 #define HEIGHT 512
+
+#define PI M_PI
 
 int mapX = 8, mapY = 8, mapS = 64;
 int map[] = {
@@ -17,7 +20,6 @@ int map[] = {
   1, 0, 0, 0, 0, 0, 0, 1,
   1, 1, 1, 1, 1, 1, 1, 1,
 };
-
 
 void drawMap2D() {
   int xo, yo;
@@ -43,33 +45,52 @@ void drawMap2D() {
 }
 
 typedef struct Player {
-  int x;
-  int y;
+  int x, y;
+  float x_delta, y_delta, angle;
 } Player;
 
-Player player = {300, 300};
+Player player;
+
 
 void drawPlayer() {
-  glColor3f(1, 1, 0);
+  glColor3f(1, 1, 1);
   glPointSize(8);
   glBegin(GL_POINTS);
   glVertex2i(player.x, player.y);
+  glEnd();
+
+  glLineWidth(3);
+  glBegin(GL_LINES);
+  glVertex2i(player.x, player.y);
+  glVertex2i(player.x + player.x_delta * 5, player.y + player.y_delta * 5);
   glEnd();
 }
 
 void handlerKeyPressed(unsigned char key, int x, int y) {
   switch (key) {
   case 'a':
-    player.x -= 5;
+    player.angle -= .1f;
+    if (player.angle < 0)
+      player.angle += PI * 2;
+
+    player.x_delta = cos(player.angle) * 5;
+    player.y_delta = sin(player.angle) * 5;
     break;
   case 'd':
-    player.x += 5;
+    player.angle += .1f;
+    if (player.angle > PI * 2)
+      player.angle -= PI * 2;
+
+    player.x_delta = cos(player.angle) * 5;
+    player.y_delta = sin(player.angle) * 5;
     break;
   case 'w':
-    player.y -= 5;
+    player.y += player.y_delta;
+    player.x += player.x_delta;
     break;
   case 's':
-    player.y += 5;
+    player.y -= player.y_delta;
+    player.x -= player.x_delta;
     break;
   }
   glutPostRedisplay();
@@ -82,12 +103,20 @@ void drawDisplay() {
   drawMap2D();
   drawPlayer();
 
+  printf("x=%i, y=%i=, dx=%f, dy=%f, pa=%f\n", player.x, player.y, player.x_delta, player.y_delta, player.angle);
+
   glutSwapBuffers();
 }
 
 void init() {
   glClearColor(.3f, .3f, .3f, 0);
   gluOrtho2D(0, WIDTH, HEIGHT, 0);
+
+  player.x = 300;
+  player.y = 300;
+  player.angle = 2 * PI;
+  player.x_delta = cos(player.angle) * 5;
+  player.y_delta = sin(player.angle) * 5;
 }
 
 int main(int argc, char *argv[]) {
