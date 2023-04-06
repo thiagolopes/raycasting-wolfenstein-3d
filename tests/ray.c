88817436fdfb2cc36d17dde5c8f4c20205b97e1e
thiagolopes <thiagolopes@protonmail.com>
2023-04-06 14:36:24 -0300

    Organize files

--- tests/ray.c
+++ tests/ray.c
@@ -1,45 +1,45 @@
-#include <math.h>
 #include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void ray_horizontal(int plx, int ply, int rtr[]){
+void ray_horizontal(int plx, int ply, int rtr[]) {
   /* 60 degree */
   float angle = 1.0471975511965976;
   int degree_angle = 60;
 
-  int px=0, py=0, dx, dy, n=64;
+  int px = 0, py = 0, dx, dy, n = 64;
 
-  if (degree_angle < 180){
+  if (degree_angle < 180) {
     /* up */
-    py = ((int)(ply/n) * n - 1);
+    py = ((int)(ply / n) * n - 1);
     dy = -64;
   }
-  if (degree_angle > 180){
+  if (degree_angle > 180) {
     exit(0);
     /* down */
-    py = ((int)(ply/n) * n + n);
+    py = ((int)(ply / n) * n + n);
     dy = 64;
   }
-  px = (int) plx + (int)((ply - py) / tan(angle));
-  dx = (int) (n / tan(angle));
+  px = (int)plx + (int)((ply - py) / tan(angle));
+  dx = (int)(n / tan(angle));
 
   rtr[0] = px;
   rtr[1] = py;
   rtr[2] = dx;
   rtr[3] = dy;
-  rtr[4] = (int)px/n;
-  rtr[5] = (int)py/n;
+  rtr[4] = (int)px / n;
+  rtr[5] = (int)py / n;
 }
 
-int main(){
-  int i=6;
+int main() {
+  int i = 6;
   int rt[i];
-  ray_horizontal(96,224,rt);
+  ray_horizontal(96, 224, rt);
   printf("%i %i %i %i %i %i\n", rt[0], rt[1], rt[2], rt[3], rt[4], rt[5]);
 
   int test_rt[] = {115, 191, 36, -64, 1, 2};
-  for (int x=0; x<i; x++){
+  for (int x = 0; x < i; x++) {
     assert(rt[x] == test_rt[x] && "Passed");
   }
 }
