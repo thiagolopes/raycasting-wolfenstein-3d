e0ed56c17f4098470717e75167e999277886e174
thiagolopes <thiagolopes@protonmail.com>
2023-05-10 17:47:52 -0300

    Code organization imgui

--- src/main.cpp
+++ src/main.cpp
@@ -174,6 +174,19 @@ void engine_SDL_OpenGL_setup(AppGame *App)
         // SDL_CaptureMouse(SDL_TRUE);
         // SDL_SetRelativeMouseMode(SDL_TRUE);
         SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+
+        // imgui
+        const unsigned char *glsl = glGetString(GL_SHADING_LANGUAGE_VERSION);
+        std::string glsl_s = std::to_string(*glsl);
+
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO &io = ImGui::GetIO();
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Contro
+        ImGui::StyleColorsDark();
+        ImGui_ImplSDL2_InitForOpenGL(sdl_window, sdl_gl_context);
+        ImGui_ImplOpenGL3_Init("#version 130");
 }
 
 bool check_map_bound_index(int index_x, int index_y, int index_max_x, int index_max_y)
@@ -257,6 +270,10 @@ void DDA_Algorith(AppGame *App, glm::fvec2 *point_collision_map, glm::fvec2 *poi
 
 void engine_SDL_OpenGL_shutdown()
 {
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplSDL2_Shutdown();
+        ImGui::DestroyContext();
+
         SDL_VideoQuit();
         SDL_GL_DeleteContext(sdl_gl_context);
         SDL_DestroyWindow(sdl_window);
@@ -407,11 +424,26 @@ void draw_3d_view_flor_and_ceil(AppGame *App)
         glEnd();
 }
 
+void draw_imgui(AppGame *App)
+{
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+        bool show_demo_window = true;
+        bool show_another_window = true;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        ImGui::ShowDemoWindow(&show_demo_window);
+        ImGui::Render();
+}
+
 void draw(AppGame *App)
 {
         draw_3d_view_flor_and_ceil(App);
         draw_3d_view_port(App);
         draw_aim(App);
+        draw_imgui(App);
 }
 
 void handle_mouse_motion(AppGame *App, SDL_Event *event)
@@ -464,19 +496,6 @@ int main(int argc, char *args[])
         engine_SDL_OpenGL_setup(&App);
         engine_SDL_OpenGL_load_textures(&App);
 
-        // imgui
-        const unsigned char *glsl = glGetString(GL_SHADING_LANGUAGE_VERSION);
-        std::string glsl_s = std::to_string(*glsl);
-
-        IMGUI_CHECKVERSION();
-        ImGui::CreateContext();
-        ImGuiIO &io = ImGui::GetIO();
-        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-        io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Contro
-        ImGui::StyleColorsDark();
-        ImGui_ImplSDL2_InitForOpenGL(sdl_window, sdl_gl_context);
-        ImGui_ImplOpenGL3_Init("#version 130");
-
         while (App.run_status) {
                 SDL_Event event;
                 while (SDL_PollEvent(&event) != 0) {
@@ -513,26 +532,13 @@ int main(int argc, char *args[])
                 /* draw here */
                 draw(&App);
 
-                // Start the Dear ImGui frame
-                ImGui_ImplOpenGL3_NewFrame();
-                ImGui_ImplSDL2_NewFrame();
-                ImGui::NewFrame();
-                bool show_demo_window = true;
-                bool show_another_window = true;
-                ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-                // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-                ImGui::ShowDemoWindow(&show_demo_window);
-
                 /* update screen */
-                ImGui::Render();
                 ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
                 SDL_GL_SwapWindow(sdl_window);
         }
 
         /* shutdown */
-        ImGui_ImplOpenGL3_Shutdown();
-        ImGui_ImplSDL2_Shutdown();
-        ImGui::DestroyContext();
+
         engine_SDL_OpenGL_shutdown();
         return 0;
 }
