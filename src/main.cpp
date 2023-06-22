876546d25afaf7cbca70a78c69689183b986f7d7
thiagolopes <thiagolopes@protonmail.com>
2023-06-22 20:20:03 -0300

    Remove engine.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,22 @@ int map[24][24] = {{8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 4, 6, 4, 4, 6, 4, 6, 4, 
 SDL_Window   *sdl_window = nullptr;
 SDL_GLContext sdl_gl_context;
 
+void draw_vertical_view(int texture, double texture_of, int pixel_start, int pixel_end, int vertical_pixel) {
+    glColor3f(1, 1, 1);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glBegin(GL_LINES);
+
+    glTexCoord2d(texture_of, 1.0);
+    glVertex2i(vertical_pixel, pixel_start);
+
+    glTexCoord2d(texture_of, 0.0);
+    glVertex2i(vertical_pixel, pixel_end);
+
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+}
+
 float normalize_rand(float rand) {
     if (rand > PI2)
         return rand - PI2;
