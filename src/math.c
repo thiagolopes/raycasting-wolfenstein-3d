31e4e2b1bd342ad7ee97e5a0d4fb82f23865accd
thiagolopes <thiagolopes@protonmail.com>
2023-11-11 17:38:20 -0300

    Create math.h and math.c - initial refactor after 3d_engine_homebrew

---
+++ src/math.c
@@ -0,0 +1,37 @@
+#include <math.h>
+#include "math.h"
+
+Point2i point2i_normalize(Point2i point){
+    int len = point2i_len(point);
+    Point2i p = {(int)point.x / len, (int)point.y / len};
+    return p;
+}
+
+Point2i point2i_sub(Point2i a, Point2i b){
+  Point2i p = {a.x - b.x, a.y - b.y};
+  return p;
+}
+
+int point2i_len(Point2i point){
+    return sqrt(point.x * point.x + point.y * point.y);
+}
+
+Point2f point2f_normalize(Point2f point) {
+    float len = point2f_len(point);
+    Point2f p = {point.x / len, point.y / len};
+    return p;
+}
+
+Point2f point2f_sub(Point2f a, Point2f b) {
+    Point2f p = {a.x - b.x, a.y - b.y};
+    return p;
+}
+
+float point2f_len(Point2f point) {
+    return sqrtf(point.x * point.x + point.y * point.y);
+}
+
+float degree_to_rad(float degree) {
+    return degree * ONE_RAD;
+}
+
---
+++ src/math.h
@@ -0,0 +1,23 @@
+#define PI 3.141592
+#define TAU 6.283185
+#define ONE_RAD 0.0174533
+
+typedef struct Point2f {
+    float x;
+    float y;
+} Point2f;
+
+typedef struct Point2i {
+    int x;
+    int y;
+} Point2i;
+
+Point2i point2i_normalize(Point2i point);
+Point2i point2i_sub(Point2i a, Point2i b);
+int point2i_len(Point2i point);
+
+Point2f point2f_normalize(Point2f point);
+Point2f point2f_sub(Point2f a, Point2f b);
+float point2f_len(Point2f point);
+
+float degree_to_rad(float degree);
