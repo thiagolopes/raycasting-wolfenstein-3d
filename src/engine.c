8bd6c9b67ab35156bb2012b848a8f1891909cb84
thiagolopes <thiagolopes@protonmail.com>
2023-11-13 14:29:32 -0300

    Update engine.c with more draw functions from main

--- src/engine.c
+++ src/engine.c
@@ -8,7 +8,7 @@ void window_vsync(bool flag){
   if (SDL_GL_SetSwapInterval((int)flag) < 0) {
     perror(SDL_GetError());
   };
-  printf("Vsync set to: %i", flag);
+  printf("Vsync set to: %i\n", flag);
 };
 
 void window_fullscreen(Window* window, bool flag){
@@ -16,20 +16,21 @@ void window_fullscreen(Window* window, bool flag){
 }
 
 /* disable the cursor too by sanity */
-void window_keep_mouse_on_center(bool flag) {
+void window_capture_cursor(bool flag){
   if (flag) {
-    SDL_ShowCursor(SDL_ENABLE);
+    SDL_ShowCursor(SDL_DISABLE);
     SDL_CaptureMouse(SDL_TRUE);
     SDL_SetRelativeMouseMode(SDL_TRUE);
     SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
   } else {
-    SDL_ShowCursor(SDL_DISABLE);
+    SDL_ShowCursor(SDL_ENABLE);
     SDL_CaptureMouse(SDL_FALSE);
     SDL_SetRelativeMouseMode(SDL_FALSE);
+    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "0");
   }
 }
 
-Window window_wake_up(char* name, int width, int height, bool fullscreen) {
+Window window_wake_up(char* name, int width, int height, bool fullscreen){
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVERYTHING) < 0) {
         perror(SDL_GetError());
     }
@@ -47,9 +48,12 @@ Window window_wake_up(char* name, int width, int height, bool fullscreen) {
     SDL_Window* window;
     SDL_GLContext gl_context;
     window = SDL_CreateWindow(name,
-                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                              width, height,
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              width,
+                              height,
                               SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
+
     if (window == NULL) {
         perror(SDL_GetError());
     }
@@ -59,7 +63,7 @@ Window window_wake_up(char* name, int width, int height, bool fullscreen) {
         perror(SDL_GetError());
     }
 
-    /* start basic ORTHO projection TODO move to shader MVP*/
+    /* start basic ORTHO projection TODO move to shader MVP or render*/
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(0, width, height, 0, 1, -1);
@@ -76,4 +80,32 @@ void window_shutdown(Window *runtime){
     SDL_GL_DeleteContext(runtime->sdl_gl_context_id);
     SDL_DestroyWindow(runtime->sdl_window_id);
     SDL_Quit();
+}
+
+void draw_rectf(Rectanglef rect, Color color){
+    glBegin(GL_QUADS);
+    glColor4ub(color.r, color.g, color.b, color.a);
+    glVertex2i(rect.x, rect.y);
+    glVertex2i(rect.x + rect.width, rect.y);
+    glVertex2i(rect.x + rect.width, rect.y + rect.height);
+    glVertex2i(rect.x, rect.y + rect.height);
+    glEnd();
+}
+
+void draw_line(float posX, float posY, float posY_end, int texture_id, float texture_offset, Color color){
+    glColor4ub(color.r, color.g, color.b, color.a);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+
+    glBegin(GL_LINES);
+
+    glTexCoord2d(texture_offset, 1.0);
+    glVertex2i(posX, posY);
+
+    glTexCoord2d(texture_offset, 0.0);
+    glVertex2i(posX, posY_end);
+
+    glEnd();
+
+    glDisable(GL_TEXTURE_2D);
 }
