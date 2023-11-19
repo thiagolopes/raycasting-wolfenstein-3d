a6016270084589b8df7e80e7d0e9a4a3b2dbecda
thiagolopes <thiagolopes@protonmail.com>
2023-11-19 18:32:02 -0300

    Initial map.h implementation

---
+++ src/map.h
@@ -0,0 +1,18 @@
+#include <stddef.h>
+
+typedef struct Grid Grid;
+typedef struct Cel Cel;
+
+struct Cel{
+  int raw_value;
+};
+
+struct Grid {
+  Cel *cels;
+  size_t rows;
+  size_t cols;
+};
+
+Grid grid_load(char* path);
+int grid_area(Grid grid);
+bool grid_index_valid(int index);
