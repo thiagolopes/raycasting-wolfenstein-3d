7243357b7341affbff9effdf813c6c5d5062a0d7
thiagolopes <thiagolopes@protonmail.com>
2023-11-24 02:04:36 -0300

    Update all the rendering algorithm!! by: https://lodev.org/cgtutor/raycasting.html

--- src/main.c
+++ src/main.c
@@ -7,8 +7,11 @@
 #include <SDL2/SDL_opengl.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <dirent.h>
+
+#include <math.h>
 
 // internal
 #include "engine.h"
@@ -16,6 +19,7 @@
 #include "colors.h"
 #include "dda.c"
 #include "map.h"
+/* #include "algorithm.h" */
 
 // imgui
 // #include "external/imgui/backends/imgui_impl_opengl3.h"
@@ -32,6 +36,13 @@ typedef struct {
 } Player_t;
 
 typedef struct {
+    Point2h pos; // start position
+    Point2h dir; // initial direction vector
+    Point2h plane; // the 2d raycaster version of camera plane
+} Player;
+Player PLAYER = {{22.0, 12}, {-1.0, 0.0}, {0.0, 0.66}};
+
+typedef struct {
     int      run_status;
     Player_t Player;
 
@@ -44,55 +55,58 @@ typedef struct {
     /* engine */
 } AppGame;
 
-void update_player(Keys* keys, Player_t* player, Grid grid) {
-    float magntude = 1;
-    if (keys->shift == 1)
-        magntude = 2;
+void update_player(Keys* keys, Player_t* player, Grid grid, float deltatime) {
+    double moveSpeed = ONE_RAD / 4 * deltatime;
+    double rotSpeed = ONE_RAD / 4 * deltatime;
 
-    float dx = 0, dy = 0;
-    float new_px = 0, new_py = 0;
+    /* update angle to use a const * delta frame */
 
     /* FIX TO USE THE RELATIVE X/Y REF BASED ON THE PLAYER VIEW */
-    if (keys->d == 1) {
-        dy -= player->direction_x * magntude;
-        dx += player->direction_y * magntude;
+    if (keys->d == 1) { // right
+        double old_dir_x = PLAYER.dir.x;
+        PLAYER.dir.x = PLAYER.dir.x * cos(-rotSpeed) - PLAYER.dir.y * sin(-rotSpeed);
+        PLAYER.dir.y = old_dir_x * sin(-rotSpeed) + PLAYER.dir.y * cos(-rotSpeed);
+
+        double old_plane_x = PLAYER.plane.x;
+        PLAYER.plane.x = PLAYER.plane.x * cos(-rotSpeed) - PLAYER.plane.y * sin(-rotSpeed);
+        PLAYER.plane.y = old_plane_x * sin(-rotSpeed) + PLAYER.plane.y * cos(-rotSpeed);
     }
     if (keys->a == 1) {
-        dy += player->direction_x * magntude;
-        dx -= player->direction_y * magntude;
+        double old_dir_x = PLAYER.dir.x;
+        //TODO this math is a matrix rotation; move to math
+        PLAYER.dir.x = PLAYER.dir.x * cos(rotSpeed) - PLAYER.dir.y * sin(rotSpeed);
+        PLAYER.dir.y = old_dir_x * sin(rotSpeed) + PLAYER.dir.y * cos(rotSpeed);
+
+        double old_plane_x = PLAYER.plane.x;
+        PLAYER.plane.x = PLAYER.plane.x * cos(rotSpeed) - PLAYER.plane.y * sin(rotSpeed);
+        PLAYER.plane.y = old_plane_x * sin(rotSpeed) + PLAYER.plane.y * cos(rotSpeed);
     }
     if (keys->w == 1) {
-        dx += player->direction_x * magntude;
-        dy += player->direction_y * magntude;
+        PLAYER.pos.x += PLAYER.dir.x * moveSpeed;
+        PLAYER.pos.y += PLAYER.dir.y * moveSpeed;
     }
     if (keys->s == 1) {
-        dx -= player->direction_x * magntude;
-        dy -= player->direction_y * magntude;
+        PLAYER.pos.x -= PLAYER.dir.x * moveSpeed;
+        PLAYER.pos.y -= PLAYER.dir.y * moveSpeed;
     }
     if (keys->j == 1) {
     }
     if (keys->k == 1) {
         SHADOWS = !SHADOWS;
     }
+/* #define H 32 */
+/* #define BODY_AREA 4 */
+/*     int o_mx = (int)(player->x / H); */
+/*     int o_my = (int)(player->y / H); */
+/*     int mx   = (int)((player->x + (dx * BODY_AREA)) / H); */
+/*     int my   = (int)((player->y + (dy * BODY_AREA)) / H); */
 
-    new_px = player->x + dx;
-    new_py = player->y + dy;
-
-#define H 32
-#define BODY_AREA 4
-    int o_mx = (int)(player->x / H);
-    int o_my = (int)(player->y / H);
-    int mx   = (int)((player->x + (dx * BODY_AREA)) / H);
-    int my   = (int)((player->y + (dy * BODY_AREA)) / H);
-
-    if (grid.cels[o_my][mx].raw_value == 0) {
-        player->x = new_px;
-    }
-    if (grid.cels[my][o_mx].raw_value == 0) {
-        player->y = new_py;
-    }
-
-    player->angle = normalize_rand(player->angle);
+/*     if (grid.cels[o_my][mx].raw_value == 0) { */
+/*         player->x = new_px; */
+/*     } */
+/*     if (grid.cels[my][o_mx].raw_value == 0) { */
+/*         player->y = new_py; */
+/*     } */
 }
 
 static void handle_key(SDL_Keysym keysym, AppGame* App, Keys* keys, int button_action) {
@@ -117,11 +131,9 @@ static void handle_key(SDL_Keysym keysym, AppGame* App, Keys* keys, int button_a
             break;
         case SDLK_j:
             keys->j = button_action;
-            keys->k = 0;
             break;
         case SDLK_k:
             keys->k = button_action;
-            keys->j = 0;
             break;
         case SDLK_LCTRL:
             keys->ctrl = button_action;
@@ -149,85 +161,159 @@ void handle_mouse_pressed_down(int button, float x, float y, AppGame* App) {
     }
 }
 
-void draw_3d_view_port(AppGame* App, Window* win, Texture* t) {
-    int pixels_cols    = win->width;
-    int draw_screen_h  = win->height;
-    int wall_height    = App->map_height;
-    int map_len        = App->map_cols;
-    int(*map)[24]      = App->map_tile;
-    int   pitch        = App->Player.pitch_view; /* player look up or down */
-    float player_angle = App->Player.angle;
-    float fov_in_rad   = to_radf(App->Player.fov);
-    float hfov_in_rad  = to_radf(App->Player.fov / 2);
-    float pixel_in_rad = fov_in_rad / pixels_cols;
 
-    Point2f point_start = {App->Player.x, App->Player.y};
-    Color   draw_color;
-    draw_color.a             = 255;
-    float fade_draw_vertical = 1.5;
+typedef struct {
+    //length of ray from current position to next x or y-side
+    Point2h side_dist;
+    //what direction to step in x or y-direction (either +1 or -1)
+    Point2i step;
+} Ray_step;
 
-    for (int pixel = 0; pixel < pixels_cols; pixel++) {
-        int     line_h;
-        int     line_start;
-        int     line_end;
-        float   texture_offset;
-        float   angle;
-        float   distance;
-        Point2f point_end;
-        DDA_t   ray_collision;
-        float   distance_len;
-        Point2f distance_xy;
+//calculate step and initial side
+Ray_step ray_create_step(Point2h pos, Point2h dir, Point2i grid, Point2h delta_dist){
+    Point2h side_dist;
+    Point2i step;
 
-        angle       = normalize_rand(normalize_rand(player_angle - hfov_in_rad) + pixel_in_rad * pixel);
-        point_end.x = point_start.x + cos(angle);
-        point_end.y = point_start.y + -sin(angle);
+    // set steps base on direction growth on the grid;
+    if (dir.x < 0) {
+            step.x = -1;
+            side_dist.x = (pos.x - grid.x) * delta_dist.x;
+    }
+    else {
+            step.x = 1;
+            side_dist.x = (grid.x + 1.0 - pos.x) * delta_dist.x;
+    }
+    // is left
+    if (dir.y < 0) {
+        step.y = -1;
+        side_dist.y = (pos.y - grid.y) * delta_dist.y;
+    }
+    // is right
+    else {
+        step.y = 1;
+        side_dist.y = (grid.y + 1.0 - pos.y) * delta_dist.y;
+    }
 
-        ray_collision = DDA(map, wall_height, map_len, point_end, point_start);
+    Ray_step ray_step = {side_dist, step};
+    return ray_step;
+}
 
-        if (ray_collision.valid) {
-            distance_xy  = point2f_sub(point_start, ray_collision.collision_point);
-            distance_len = point2f_len(distance_xy);
-            distance     = fix_eye_fish(distance_xy, player_angle - angle);
+// walk ray to the next x and y map grid;
+void ray_next_step_on(Point2h delta_dist, Point2i step, Point2h* side_dist, Point2i* map_grid, int* side) {
+    // jump to next map square, either in x-direction, or in y-direction
+    if (side_dist->x < side_dist->y) {
+        side_dist->x += delta_dist.x;
+        map_grid->x += step.x;
+        *side = 0;
+    } else {
+        side_dist->y += delta_dist.y;
+        map_grid->y += step.y;
+        *side = 1;
+    }
+};
 
-            line_h     = (wall_height * draw_screen_h) / distance;
-            line_start = (draw_screen_h / 2 - line_h / 2) - pitch;
-            line_end   = line_start + line_h;
 
-            float color;
-            if (SHADOWS){
-                #define SHADOW_MAX 500.0
-                #define NEAR 0.1
-                #define FAR 0.70
-                float dd = distance_len / SHADOW_MAX;
-                float cp = 1.0 - exp(-dd * dd / (FAR * NEAR));
-                color    = 255 - 255 * cp;
-            } else {
-                color = 255;
-            }
-            draw_color.r = color;
-            draw_color.g = color;
-            draw_color.b = color;
+void draw_ray(){
 
-            if (ray_collision.side == HORIZONTAL) {
-                texture_offset = (float)((int)(ray_collision.collision_point.x) % wall_height) / wall_height;
-            } else {
-                texture_offset = (float)((int)(ray_collision.collision_point.y) % wall_height) / wall_height;
-            }
-            draw_line_vertical(pixel, line_start, line_end, ray_collision.grid_index_collision, texture_offset,
-                               draw_color);
+};
+
+void new_3d_render(AppGame* app, Grid* map){
+    Player r = PLAYER;
+    #define w 1920
+    #define h 1080
+
+
+    for(size_t x = 0; x < w; x++){
+        //x-coordinate in camera space
+        double camera_pos = 2 * x / (double)w - 1;
+        //calculate ray position and direction
+        Point2h ray_dir = {r.dir.x + r.plane.x * camera_pos, r.dir.y + r.plane.y * camera_pos};
+        //which box of the map ray are in
+        Point2i map_grid = {(int)r.pos.x, (int)r.pos.y};
+        //length of ray from one x or y-side to next x or y-side
+        Point2h delta_dist = {fabs(1 / ray_dir.x), fabs(1 / ray_dir.y)};
+        //lenght of the ray while steps;
+        double ray_dist;
+
+        bool hit = false; //was there a wall hit?
+        int side; //was a NS or a EW wall hit?
+
+        Ray_step s = ray_create_step(PLAYER.pos, ray_dir, map_grid, delta_dist);
+        Point2i step = s.step;
+        Point2h side_dist = s.side_dist;
+
+        if (ray_dir.x < 0) {
+            step.x = -1;
+            side_dist.x = (r.pos.x - map_grid.x) * delta_dist.x;
+        } else {
+            step.x = 1;
+            side_dist.x = (map_grid.x + 1.0 - r.pos.x) * delta_dist.x;
         }
+        if (ray_dir.y < 0) {
+            step.y = -1;
+            side_dist.y = (r.pos.y - map_grid.y) * delta_dist.y;
+        } else {
+            step.y = 1;
+            side_dist.y = (map_grid.y + 1.0 - r.pos.y) * delta_dist.y;
+        }
+
+        while(!hit){
+            ray_next_step_on(delta_dist, step, &side_dist, &map_grid, &side);
+            if (grid_index_valid(*map, map_grid.x, map_grid.y)){
+                if (map->cels[map_grid.x][map_grid.y].raw_value != 0) {
+                    hit = true;
+                }
+            }
+        }
+
+        // camera plane dist
+        if(side == 0){
+            ray_dist = (side_dist.x - delta_dist.x);
+        }
+        else{
+            ray_dist = (side_dist.y - delta_dist.y);
+        }
+        // move to draw_ray
+        //Calculate height of line to draw on screen
+        int wall_height = (int)(h / ray_dist);
+        // calculate where the line start and ends, talken h as center of hall;
+        int draw_start = -wall_height / 2 + h / 2;
+        int draw_end = wall_height / 2 + h / 2;
+        /* if (draw_start < 0) */
+            /* draw_start = 0; */
+        /* if (draw_end >= h) */
+            /* draw_end = h - 1; */
+
+      double wall_x;
+      if (side == 0)
+          wall_x = PLAYER.pos.y + ray_dist * ray_dir.y;
+      else
+          wall_x = PLAYER.pos.x + ray_dist * ray_dir.x;
+      wall_x -= floor((wall_x));
+
+      #define SHADOW_MAX 25
+      #define NEAR 0.1
+      #define FAR 0.70
+      Color   draw_color = {0};
+      float dd = ray_dist / SHADOW_MAX;
+      float cp = 1.0 - exp(-dd * dd / (FAR * NEAR));
+      float color  = 255 - 255 * cp;
+      draw_color.r = color;
+      draw_color.g = color;
+      draw_color.b = color;
+
+      draw_line_vertical(x, draw_start, draw_end, map->cels[map_grid.x][map_grid.y].raw_value, wall_x, draw_color);
     }
 }
 
 void draw_3d_view_floor(AppGame* App, Window* win) {
-    float      floor_start = win->height / 2 - App->Player.pitch_view;
+    float      floor_start = win->height / 2;
     Rectanglef floor       = {0, floor_start, (float)win->width, (float)win->height + App->Player.pitch_view};
     draw_rectf_gradient(floor, BLACK, DARKGRAY);
 }
 
-void draw(AppGame* App, Window* win, Texture* texture) {
+void draw(AppGame* App, Window* win, Grid* grid) {
     draw_3d_view_floor(App, win);
-    draw_3d_view_port(App, win, texture);
     draw_aim(win);
 }
 
@@ -243,10 +329,6 @@ void handle_mouse_motion(AppGame* App, SDL_Event* event) {
 
     switch (event->type) {
         case SDL_MOUSEMOTION:
-            App->Player.pitch_view += (event->motion.yrel * 3);
-            App->Player.angle += MOUSE_VELOCITY * event->motion.xrel;
-            App->Player.direction_x = cos(App->Player.angle);
-            App->Player.direction_y = -sin(App->Player.angle);
             break;
     }
 }
@@ -275,6 +357,7 @@ int main(int argc, char* args[]) {
     Texture  t8          = texture_new("textures/wall_damage_c.png", false);
     Texture  t9          = texture_new("textures/test.png", false);
     Texture* textures[9] = {&t1, &t2, &t3, &t4, &t5, &t6, &t7, &t8, &t9};
+    // TODO tab enable select texture
 
     Grid grid = grid_load("maps/map_01");
     for (int x = 0; x < App.map_cols; x++) {
@@ -323,12 +406,12 @@ int main(int argc, char* args[]) {
             }
         }
 
-        /* if (delta > frame_delta) { */
             /* update here */
-            update_player(&keys_map, &App.Player, grid);
+            update_player(&keys_map, &App.Player, grid, delta);
 
             /* draw here */
-            draw(&App, &window_deamon, textures);
+            draw(&App, &window_deamon, &grid);
+            new_3d_render(&App, &grid);
 
             {
                 texture_bind(t9);
@@ -339,6 +422,7 @@ int main(int argc, char* args[]) {
 
             last_time = current_time;
             ++frame_counter;
+        /* if (delta > frame_delta) { */
         /* } */
     }
 
